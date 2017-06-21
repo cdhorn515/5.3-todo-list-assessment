a15f8920a2e95845ff58e29b6715febe2f2012d5
Christina Cornell <cdhorn515@gmail.com>
2017-06-21 19:19:21 -0400

    finished project

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
+// const jsdom = require('jsdom');
+var qu
 
 const app = express();
 
@@ -12,29 +14,45 @@ app.set('views', './views');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+var todoIdx = 0;
 
-const context = {
+var context = {
   todo: [
   'Buy groceries'
   , 'Fold laundry'
   , 'Wash the dog'
 ]
+, todoId: function(){
+  return todoIdx++;
+}
 , completed: [
   'Clean kitchen'
   ,'Do homework'
 ]
+, completedId: function(){
+  return completedIdx++;
+}
 };
 
-var todoItems = context.todo;
-
 app.get('/', function(req, res){
+   todoIdx = 0;
   res.render('index', context);
 });
 
 app.post('/', function (req, res){
+  var todoItems = context.todo;
   //pushes into variable that I created
   todoItems.push(req.body.to_do);
-  res.render('index', context);
+  res.redirect('/');
+});
+
+app.post('/todo/:id', function (req, res){
+   console.log("working");
+   var id = req.params.id; //get address, look up, add to completed, delete from todo
+   var todo = context.todo.splice(id, 1);
+   context.completed.push(todo);
+   res.redirect('/');
+
 });
 
 app.listen(3000, function(){
