f68486e2e3bbb3568183bbd88f41d20145219088
Christina Cornell <cdhorn515@gmail.com>
2017-06-22 10:23:44 -0400

    removed random incomplete var

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 // const jsdom = require('jsdom');
-var qu
+
 
 const app = express();
 
