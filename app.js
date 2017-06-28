51e74fcb96467e19ebae87a37c78b072ee2b52e0
Christina Cornell <cdhorn515@gmail.com>
2017-06-28 14:58:42 -0400

    using sequelize db to add items to todo list

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 // const jsdom = require('jsdom');
-
+const models = require('./models');
 
 const app = express();
 
@@ -16,45 +16,80 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 var todoIdx = 0;
 
-var context = {
-  todo: [
-  'Buy groceries'
-  , 'Fold laundry'
-  , 'Wash the dog'
-]
-, todoId: function(){
-  return todoIdx++;
-}
-, completed: [
-  'Clean kitchen'
-  ,'Do homework'
-]
-, completedId: function(){
-  return completedIdx++;
-}
-};
+// models.Todo.create({
+//   title: "buy groceries",
+//   priority: 4,
+//   "due_date": new Date(2017, 5, 30),
+// });
+
+// models.Todo.create({
+//   title: "fold laundry",
+//   "due_date": new Date(2017, 6, 1),
+//   completed: true
+// });
+
+// models.Todo.create({
+//   title: "wash the dog",
+//   "due_date": new Date(2017, 6, 12),
+//   completed: false
+// });
+//
+// models.Todo.create({
+//   title: "clean kitchen",
+//   "due_date": new Date(2017, 5, 29),
+//   completed: true
+// });
+
 
 app.get('/', function(req, res){
-   todoIdx = 0;
-  res.render('index', context);
+  //  todoIdx = 0;
+  models.Todo.findAll().then(function(todos){
+    res.render('index', {model: todos});
+  });
 });
 
 app.post('/', function (req, res){
-  var todoItems = context.todo;
-  //pushes into variable that I created
-  todoItems.push(req.body.to_do);
+  models.Todo.create({
+    title: req.body.title,
+    priority: req.body.priority,
+    due_date: req.body.due_date,
+    completed: req.body.completed,
+    assignee: req.body.assignee
+  });
+  // var todoItems = context.todo;
+  // //pushes into variable that I created
+  // todoItems.push(req.body.to_do);
   res.redirect('/');
 });
 
-app.post('/todo/:id', function (req, res){
-   console.log("working");
-   var id = req.params.id; //get address, look up, add to completed, delete from todo
-   var todo = context.todo.splice(id, 1);
-   context.completed.push(todo);
-   res.redirect('/');
-
-});
+// app.post('/todo/:id', function (req, res){
+//    console.log("working");
+//    var id = req.params.id; //get address, look up, add to completed, delete from todo
+//    var todo = context.todo.splice(id, 1);
+//    context.completed.push(todo);
+//    res.redirect('/');
+//
+// });
 
 app.listen(3000, function(){
 console.log("app started successfully!");
 });
+
+
+// var context = {
+//   todo: [
+//   'Buy groceries'
+//   , 'Fold laundry'
+//   , 'Wash the dog'
+// ]
+// , todoId: function(){
+//   return todoIdx++;
+// }
+// , completed: [
+//   'Clean kitchen'
+//   ,'Do homework'
+// ]
+// , completedId: function(){
+//   return completedIdx++;
+// }
+// };
---
+++ models/todo.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Todo = sequelize.define('Todo', {
+    title: DataTypes.STRING,
+    priority: DataTypes.INTEGER,
+    due_date: DataTypes.DATEONLY,
+    completed: DataTypes.BOOLEAN
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Todo;
+};
